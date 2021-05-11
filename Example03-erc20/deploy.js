6aa3cab7c8e587fbed2d7cdfb5fabd7b4feef152
yanyanho <yanyanho@126.com>
2021-05-11 10:51:30 +0800

    add transfer transaction

--- Example03-erc20/deploy.js
+++ Example03-erc20/deploy.js
@@ -63,6 +63,33 @@ const deploy = async () => {
    console.log(
       `Contract deployed at address: ${createReceipt.contractAddress}`
    );
+
+
+   const newbac = new web3.eth.Contract(abi, createReceipt.contractAddress);
+
+   const shortname = newbac.methods.shortName().call().then(console.log);
+
+  // newbac.methods.send("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",100000, "fee").send({from: '0x54A65DB20D7653CE509d3ee42656a8F138037d51'}).then(console.log);
+
+   //build the Tx
+   const tx = newbac.methods.send("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",100000, "fee").encodeABI();
+
+   // Sign Tx with PK
+   const createTransaction1 = await web3.eth.accounts.signTransaction(
+       {
+          to: createReceipt.contractAddress,
+          data: tx,
+          gas: 8000000,
+       },
+       account_from.privateKey
+   );
+
+   // Send Tx and Wait for Receipt
+   const createReceipt1 = await web3.eth.sendSignedTransaction(
+       createTransaction1.rawTransaction
+   );
+
+   newbac.methods.balance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266").call().then(console.log)
 };
 
 deploy();
