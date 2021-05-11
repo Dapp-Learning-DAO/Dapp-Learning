abd5209fcfd2ff38d6274b1fce65eb201012e4e4
yanyanho <yanyanho@126.com>
2021-05-11 22:36:23 +0800

    fix the test pass

--- Example05-ethersjs-waffle/index.js
+++ Example05-ethersjs-waffle/index.js
@@ -8,14 +8,13 @@ const Web3 = require('web3');
     const privateKey = fs.readFileSync("./sk.txt").toString().trim()
 
 
-    const web3 = new Web3.providers.HttpProvider('https://kovan.infura.io/v3/0aae8358bfe04803b8e75bb4755eaf07');
-
-    let web3Provider = new ethers.providers.Web3Provider(web3)
-
-    let wallet = new ethers.Wallet(privateKey);
+    // const web3 = new Web3.providers.HttpProvider('https://kovan.infura.io/v3/0aae8358bfe04803b8e75bb4755eaf07');
+    //  let web3Provider = new ethers.providers.Web3Provider(web3)
 
 
- //   provider = new ethers.providers.InfuraProvider(  "kovan","0aae8358bfe04803b8e75bb4755eaf07"  )
+    const web3Provider = new ethers.providers.InfuraProvider(  "kovan","0aae8358bfe04803b8e75bb4755eaf07"  )
+
+    const wallet = new ethers.Wallet(privateKey,web3Provider);
 
     let address = "0x54A65DB20D7653CE509d3ee42656a8F138037d51";
 
@@ -28,36 +27,39 @@ const Web3 = require('web3');
         // balance is a BigNumber (in wei); format is as a sting (in ether)
         let etherString = ethers.utils.formatEther(balance);
         bal = balance;
-
         console.log("Balance: " + etherString);
     });
 
-
-
-
+   //  todo
+    // async function balance(address) {
+   //     await  web3Provider.getBalance(address).then((balance) => {
+   //
+   //         // balance is a BigNumber (in wei); format is as a sting (in ether)
+   //         let etherString = ethers.utils.formatEther(balance);
+   //         return etherString
+   //     });
+   // }
+   // let ethBalance   =   balance(address);
+   //  console.log("Balance: " + ethBalance);
     let token;
 
-    (async function() {
+    async function deploy() {
 
         // 常见合约工厂实例
-       try {
            const simpletoken = new ethers.ContractFactory(SimpleToken.abi, SimpleToken.bytecode, wallet);
            token = await simpletoken.deploy("HEHE", "HH", 1, 100000000);
-       } catch (error) {
-           console.log(error)
-       }
-
-        // 部署交易有一旦挖出，合约地址就可用
         console.log(token.address);
 
-       // console.log(token.deployTransaction.hash);
+        console.log(token.deployTransaction.hash);
 
         await token.deployed()
-        console.log( token.balanceOf(wallet.address));
 
-    })();
+        let bal =  await token.balanceOf(wallet.address);
+        console.log( bal);
 
+    };
 
+deploy();
 
 // const incrementer = new ethers.ContractFactory(abi, bytecode, wallet);
 //
