c31e282e28fd860a3348445c0414961af2c187f0
yanyanho <yanyanho@126.com>
2021-05-24 22:28:38 +0800

    add auction-fix-price test

--- Example10-hardhat-ipfs-erc721/contracts/IERC721.sol
+++ Example10-hardhat-ipfs-erc721/contracts/IERC721.sol
@@ -1,6 +1,17 @@
 pragma solidity ^0.4.25;
 
-import "./IERC165.sol";
+
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
 
 /**
  * @title ERC721 Non-Fungible Token Standard basic interface
