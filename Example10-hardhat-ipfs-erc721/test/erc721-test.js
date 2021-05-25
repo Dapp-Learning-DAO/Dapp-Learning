f3ded892e3a6e8fa325d169f1ef6cc202f3ce7a3
yanyanho <yanyanho@126.com>
2021-05-25 10:34:05 +0800

    fix the erc721

--- Example10-hardhat-ipfs-erc721/test/erc721-test.js
+++ Example10-hardhat-ipfs-erc721/test/erc721-test.js
@@ -5,12 +5,14 @@ const { exec } = require('child_process');
 describe("ERC721 contract", function() {
   it("Deployment should not assign any Token to deployer", async function() {
     const [owner,addr1, addr2] = await ethers.getSigners();
+
     console.log(owner.address)
     console.log(addr1.address)
 
     const Token = await ethers.getContractFactory("ERC721");
 
     const hardhatToken = await Token.deploy("ERC721","Token");
+    console.log(hardhatToken.address);
 
     const ownerBalance = await hardhatToken.balance(owner.address);
     expect("0").to.equal(ownerBalance);
