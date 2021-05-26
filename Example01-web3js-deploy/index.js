f012c5fa2f5bb17a5d9bb77a97bd217f973113f9
longdacao <longdacao@163.com>
2021-05-26 10:50:01 +0800

    change comment

--- Example01-web3js-deploy/index.js
+++ Example01-web3js-deploy/index.js
@@ -2,13 +2,13 @@ let Web3 = require('web3');
 let solc = require("solc");
 let fs   = require('fs');
 
-// 从 sk.txt 中获取账户私钥。 需要手工创建 sk.txt , 并放入账户私钥
+// Get privatekey from sk.txt
 const privatekey = fs.readFileSync("./sk.txt").toString().trim()
 
-// 以 utf8 方式加载合约
+// Load contract
 const source = fs.readFileSync('Incrementer.sol','utf8');
 
-// 编译合约
+// compile solidity
 const input = {
    language: 'Solidity',
    sources: {
@@ -29,14 +29,14 @@ const tempFile = JSON.parse(solc.compile(JSON.stringify(input)));
 const contractFile = tempFile.contracts['Incrementer.sol']['Incrementer'];
 
 
-// 获取 abi 和 bin
+// Get bin & abi 
 const bytecode = contractFile.evm.bytecode.object;
 const abi = contractFile.abi;
 
-// 传入 kovan 测试网地址，构造 web3 实例
+// Create web3 with kovan provider
 const web3 = new Web3('https://kovan.infura.io/v3/0aae8358bfe04803b8e75bb4755eaf07'); 
 
-// 根据账户私钥创建账户
+// Create account from privatekey
 const account = web3.eth.accounts.privateKeyToAccount(privatekey);
 const account_from = {
 	privateKey: privatekey,
@@ -44,20 +44,20 @@ const account_from = {
  };
 
 /*
-   -- 部署合约 --
+   -- Deploy Contract --
 */
 const Deploy = async () => {
 
-	// 创建合约实例
+	// Create contract instance
 	const deployContract = new web3.eth.Contract(abi);
 
-	// 构造部署合约的交易
+	// Create Tx
 	const deployTx = deployContract.deploy({
 	data: bytecode,
 	arguments: [5],
 	});
 
-	// 使用账户私钥对交易进行签名
+	// Sign Tx 
 	const deployTransaction = await web3.eth.accounts.signTransaction(
 	{
 		data: deployTx.encodeABI(),
@@ -66,7 +66,7 @@ const Deploy = async () => {
 	account_from.privateKey
 	);
 
-	// 发送合约部署交易，同时获取交易回执，并打印回执中返回的交易部署成功后的合约地址
+
 	const deployReceipt = await web3.eth.sendSignedTransaction(
 		deployTransaction.rawTransaction
 	);
--- Example02-web3js-transaction/index.js
+++ Example02-web3js-transaction/index.js
@@ -5,7 +5,7 @@ const contractFile = require('./compile');
 const privatekey = fs.readFileSync("./sk.txt").toString().trim()
 
 /*
-   -- 定义 Provider --
+   -- Define Provider --
 */
 // Provider
 const providerRPC = {
@@ -14,21 +14,21 @@ const providerRPC = {
 };
 const web3 = new Web3(providerRPC.development); //Change to correct network
 
-// 使用私钥生成账户
+// Create account with privatekey
 const  account = web3.eth.accounts.privateKeyToAccount(privatekey);
 const account_from = {
    privateKey: privatekey,
    accountAddress: account.address,
 };
 
-// 获取 abi 和 bin
+// Get abi & bin
 const bytecode = contractFile.evm.bytecode.object;
 const abi = contractFile.abi;
 
 /*
 *
 *
-*   -- 合约部署验证 --
+*   -- Verify Deployment --
 *
 
 */
@@ -36,16 +36,16 @@ const Trans = async () => {
    console.log("============================ 1. Deploy Contract");
    console.log(`Attempting to deploy from account ${account.address}`);
 
-   // 创建合约实例
+   // Create Contract Instance
    const deployContract = new web3.eth.Contract(abi);
 
-   // 构造部署合约的交易
+   // Create Deployment Tx
    const deployTx = deployContract.deploy({
       data: bytecode,
       arguments: [5],
    });
 
-   // 使用私钥对交易进行签名
+   // Sign Tx
    const createTransaction = await web3.eth.accounts.signTransaction(
       {
          data: deployTx.encodeABI(),
@@ -54,7 +54,7 @@ const Trans = async () => {
       account_from.privateKey
    );
 
-   // 发送合约部署交易，同时获取交易回执，并打印回执中返回的交易部署成功后的合约地址
+   // Get Transaction Receipt
    const createReceipt = await web3.eth.sendSignedTransaction(
       createTransaction.rawTransaction
    );
@@ -67,28 +67,27 @@ const Trans = async () => {
    *
    *
    * 
-   * -- 验证合约的 increment 接口 --
+   * -- Verify Interface of Increment --
    * 
    * 
    */
-   // 这里使用已经部署的合约地址进行构造
+   // Create the contract with contract address
    console.log()
    console.log("============================ 2. Call Contract Interface getNumber");
    let incrementer = new web3.eth.Contract(abi, createReceipt.contractAddress);
    
    console.log(`Making a call to contract at address: ${createReceipt.contractAddress}`);
 
-   // 调用合约的 getNumber 接口，获取合约中的公共变量 number 的当前值
    let number = await incrementer.methods.getNumber().call();
    console.log(`The current number stored is: ${number}`);
 
-   // 构造交易，调用合约的另一个接口 increment, 为 number + 3
+   // Add 3 to Contract Public Variable
    console.log()
    console.log("============================ 3. Call Contract Interface increment");
    const _value = 3;
    const incrementTx = incrementer.methods.increment(_value);
 
-   // 使用私钥对交易进行签名
+   // Sign with Pk
    let incrementTransaction = await web3.eth.accounts.signTransaction(
       {
          to: createReceipt.contractAddress,
@@ -98,7 +97,7 @@ const Trans = async () => {
       account_from.privateKey
    );
 
-   // 发送交易，同时获取交易回执，答应回执中的交易 hash 
+   // Send Transactoin and Get TransactionHash
    const incrementReceipt = await web3.eth.sendSignedTransaction(
       incrementTransaction.rawTransaction
    );
@@ -112,7 +111,7 @@ const Trans = async () => {
    *
    *
    * 
-   * -- 验证合约的 reset 接口 --
+   * -- Verify Interface of Reset --
    * 
    * 
    */
@@ -120,7 +119,6 @@ const Trans = async () => {
   console.log("============================ 4. Call Contract Interface reset");
   const resetTx = incrementer.methods.reset();
 
-  // 使用私钥对交易进行签名
   const resetTransaction = await web3.eth.accounts.signTransaction(
    {
       to: createReceipt.contractAddress,
@@ -130,7 +128,6 @@ const Trans = async () => {
    account_from.privateKey
    );
 
-   // 发送交易，同时获取交易回执，答应回执中的交易 hash 
    const resetcReceipt = await web3.eth.sendSignedTransaction(
       resetTransaction.rawTransaction
    );
@@ -143,7 +140,7 @@ const Trans = async () => {
    *
    *
    * 
-   * -- 监听合约 Increment 事件 --
+   * -- Listen to Event Increment --
    * 
    * 
    */
@@ -151,12 +148,12 @@ const Trans = async () => {
    console.log("============================ 5. Call Contract Interface reset");
    console.log("Mode 1: listen to event once")
 
-   // kovan 测试网无法使用 http 进行 event 事件监听，需要使用 websocket
-   // 具体可参考 https://medium.com/blockcentric/listening-for-smart-contract-events-on-public-blockchains-fdb5a8ac8b9a
+   // kovan don't support http protocol to event listen, need to use websocket
+   // more details , please refer to  https://medium.com/blockcentric/listening-for-smart-contract-events-on-public-blockchains-fdb5a8ac8b9a
    const web3Socket = new Web3(new Web3.providers.WebsocketProvider("wss://kovan.infura.io/ws/v3/0aae8358bfe04803b8e75bb4755eaf07"));
    incrementer = new web3Socket.eth.Contract(abi, createReceipt.contractAddress);
 
-   // 只监听一次 Increment 事件
+   // listen to  Increment event only once
    incrementer.once('Increment', (error,event) => {
       console.log('Get event Increment, and the increment value is ', event.returnValues.value)
       process.exit(0)
