6879dda25e3e4285ba6d8c607949e40908feb650
yingjingyang <longdacao2@163.com>
2021-06-09 20:02:17 +0800

    change erc20 to the latest one

--- Example05-ethersjs-erc20/compile.js
+++ Example05-ethersjs-erc20/compile.js
@@ -1,30 +1,28 @@
-const fs = require('fs');
-const solc = require('solc');
+const fs = require('fs')
+const solc = require('solc')
 
 // Get Path and Load Contract
-const source = fs.readFileSync('SimpleToken.sol','utf8');
+const source = fs.readFileSync('SimpleToken.sol', 'utf8')
 
 // Compile Contract
 const input = {
-   language: 'Solidity',
-   sources: {
-      'SimpleToken.sol': {
-         content: source,
+  language: 'Solidity',
+  sources: {
+    'SimpleToken.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
       },
-   },
-   settings: {
-      outputSelection: {
-         '*': {
-            '*': ['*'],
-         },
-      },
-   },
-};
+    },
+  },
+}
 
-
-const tempFile = JSON.parse(solc.compile(JSON.stringify(input)));
-const contractFile = tempFile.contracts['SimpleToken.sol']['SimpleToken'];
-
+const tempFile = JSON.parse(solc.compile(JSON.stringify(input)))
+const contractFile = tempFile.contracts['SimpleToken.sol']['SimpleToken']
 
 // Export Contract Data
-module.exports = contractFile;
+module.exports = contractFile
