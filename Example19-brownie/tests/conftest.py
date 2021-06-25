d53738de1be43e66b6e0b8cb1ea88d8f714c97ae
John Hush <pitaloveu8@gmail.com>
2021-06-26 01:07:57 +0800

    add factory initialization test function

--- Example19-brownie/tests/conftest.py
+++ Example19-brownie/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from web3 import Web3
 from web3.contract import ConciseContract
-from brownie import TestToken, accounts
+from brownie import SimpleToken, UniswapFactory, accounts
 
 # reference to .
 # https://github.com/Uniswap/uniswap-v1/blob/master/tests/conftest.py
@@ -17,16 +17,31 @@ def w3():
 
 
 @pytest.fixture
-def HAY_token(w3):
-    t = TestToken.deploy("HAYAHEI", "HAY", 18, {"from": accounts[0]})
-    # print(w3.eth.default_account)
+def HAY_token():
+    t = SimpleToken.deploy("HAYAHEI", "HAY", 18, 0, {"from": accounts[0]})
     # print(w3.eth.accounts[0])
-    # print(accounts[0])
 
     # mint 500 to account0 and account1, respectively
     t.mint(accounts[0], 500 * 10**18, {"from": accounts[0]})
     t.mint(accounts[1], 500 * 10**18, {"from": accounts[0]})
 
-    # x = t.balanceOf(accounts[0])
-    # print(x)
+    return t
+
+
+@pytest.fixture
+def BEE_token():
+    t = SimpleToken.deploy("BEEEEEE", "BEE", 18, 0, {"from": accounts[0]})
+    # print(w3.eth.accounts[0])
+
+    # mint 500 to account0 and account1, respectively
+    t.mint(accounts[0], 500 * 10**18, {"from": accounts[0]})
+    t.mint(accounts[1], 500 * 10**18, {"from": accounts[0]})
+
+    return t
+
+
+@pytest.fixture
+def uniswap_factory():
+    t = UniswapFactory.deploy({"from": accounts[0]})
+
     return t
--- Example19-brownie/tests/exchange/test_token.py
+++ Example19-brownie/tests/exchange/test_token.py
@@ -7,6 +7,7 @@ def test_initial_state(HAY_token):
     assert HAY_token.decimals() == 18
     assert HAY_token.totalSupply() == 1000 * 10**18
 
+
 def test_ERC20(HAY_token):
     a0 = accounts[0]
     a1 = accounts[1]
@@ -17,23 +18,14 @@ def test_ERC20(HAY_token):
     assert HAY_token.balanceOf(a0) == 499*10**18
     assert HAY_token.balanceOf(a1) == 501*10**18
 
-# def test_mint_transfer(t, uni_token, assert_tx_failed):
-#     # Mint fails if not called by contact owner
-#     assert_tx_failed(t, lambda: uni_token.mint(t.a2, 100, sender=t.k2))
-#     assert uni_token.balanceOf(t.a0) == 0
-#     uni_token.mint(t.a1, 100)
-#     uni_token.mint(t.a2, 200)
-#     assert uni_token.balanceOf(t.a1) == 100
-#     assert uni_token.balanceOf(t.a2) == 200
-#     assert uni_token.totalSupply() == 300
-#     uni_token.transfer(t.a2, 100, sender=t.k1)
-#     assert uni_token.balanceOf(t.a1) == 0
-#     assert uni_token.balanceOf(t.a2) == 300
-#     assert uni_token.totalSupply() == 300
-#     # transferFrom fails if not approved
-#     assert_tx_failed(t, lambda: uni_token.transferFrom(t.a2, t.a1, 200, sender=t.k3))
-#     uni_token.approve(t.a3, 200, sender=t.k2)
-#     uni_token.transferFrom(t.a2, t.a1, 200, sender=t.k3)
-#     assert uni_token.balanceOf(t.a1) == 200
-#     assert uni_token.balanceOf(t.a2) == 100
-#     assert uni_token.totalSupply() == 300
+
+def test_transfer_approval(HAY_token):
+    uni_token = HAY_token
+
+    assert uni_token.balanceOf(accounts[0]) == 500 * 10**18
+    assert uni_token.balanceOf(accounts[1]) == 500 * 10**18
+
+    uni_token.approve(accounts[2], 200 * 10**18, {"from": accounts[0]})
+    uni_token.transferFrom( accounts[0], accounts[9], 5 * 10**18, {"from": accounts[2]})
+    assert uni_token.balanceOf(accounts[0]) == 495 * 10**18
+    assert uni_token.balanceOf(accounts[9]) == 5 * 10**18
