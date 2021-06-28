974e5a458aea10d94f3673874df3f9e87067cea8
maroonstar <744428333@qq.com>
2021-06-28 13:49:58 +0800

    uniswap-v1-like update : how to calculate amount

--- Example13-exhange/uniswap-v1-like/contracts/Exchange.sol
+++ Example13-exhange/uniswap-v1-like/contracts/Exchange.sol
@@ -21,15 +21,32 @@ contract Exchange {
         return IERC20(tokenAddress).balanceOf(address(this));
     }
 
-    function getPrice(uint256 inputReserve, uint256 outputReserve)
-        public
-        pure
-        returns (uint256)
-    {
+    // This is a low-level function, so let it be private.
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256) {
         require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
 
-        // Solidity supports integer division with only floor rounding.
-        // fix that by increasing the precision.
-        return (inputReserve * 1000) / outputReserve;
+        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
+    // This is a low-level function, so let it be private.
+    function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
+        require(_ethSold > 0, "ethSold is too small");
+
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_ethSold, address(this).balance, tokenReserve);
+    }
+
+    // This is a low-level function, so let it be private.
+    function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
+        require(_tokenSold > 0, "tokenSold is too small");
+
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_tokenSold, tokenReserve, address(this).balance);
     }
 }
--- Example13-exhange/uniswap-v1-like/test/Exchange.test.js
+++ Example13-exhange/uniswap-v1-like/test/Exchange.test.js
@@ -35,21 +35,35 @@ describe("Exchange", async () => {
     })
   })
 
-  describe("getPrice", async () => {
-    it("returns correct prices", async () => {
+  describe("getTokenAmount", async () => {
+    it("returns correct token amount", async () => {
       await token.approve(exchange.address, toWei(2000))
       await exchange.addLiquidity(toWei(2000), { value: toWei(1000) })
 
-      const tokenReserve = await exchange.getReserve()
-      const etherReserve = await getBalance(exchange.address)
+      let tokensOut = await exchange.getTokenAmount(toWei(1))
+      expect(fromWei(tokensOut)).to.equal("1.998001998001998001")
 
-      // ETH per token
-      expect(
-        (await exchange.getPrice(etherReserve, tokenReserve)).toString()
-      ).to.eq("0.5")
+      tokensOut = await exchange.getTokenAmount(toWei(100))
+      expect(fromWei(tokensOut)).to.equal("181.818181818181818181")
 
-      // token per ETH
-      expect(await exchange.getPrice(tokenReserve, etherReserve).to.eq("0.5"))
+      tokensOut = await exchange.getTokenAmount(toWei(1000))
+      expect(fromWei(tokensOut)).to.equal("1000.0")
+    })
+  })
+
+  describe("getEthAmount", async () => {
+    it("returns correct ether amount", async () => {
+      await token.approve(exchange.address, toWei(2000))
+      await exchange.addLiquidity(toWei(2000), { value: toWei(1000) })
+
+      let ethOut = await exchange.getEthAmount(toWei(2))
+      expect(fromWei(ethOut)).to.equal("0.999000999000999")
+
+      ethOut = await exchange.getEthAmount(toWei(100))
+      expect(fromWei(ethOut)).to.equal("47.619047619047619047")
+
+      ethOut = await exchange.getEthAmount(toWei(2000))
+      expect(fromWei(ethOut)).to.equal("500.0")
     })
   })
 })
