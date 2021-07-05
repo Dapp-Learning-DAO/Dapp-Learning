e19699ac447854e503201e093dcc7f000e72ff7f
maroonstar <744428333@qq.com>
2021-07-05 19:40:59 +0800

    test-helpers

--- Example10-hardhat-ipfs-erc721/test/erc721-test.js
+++ Example10-hardhat-ipfs-erc721/test/erc721-test.js
@@ -1,21 +1,41 @@
+require("@nomiclabs/hardhat-waffle");
 const { expect } = require("chai");
-const { exec } = require('child_process');
+const {
+  BN,           // Big Number support
+  constants,    // Common constants, like the zero address and largest integers
+  expectEvent,  // Assertions for emitted events
+  expectRevert, // Assertions for transactions that should fail
+} = require('@openzeppelin/test-helpers');
+
+const toWei = (value) => ethers.utils.parseEther(value.toString());
+
+const fromWei = (value) =>
+  ethers.utils.formatEther(
+    typeof value === "string" ? value : value.toString()
+  );
+
+const getBalance = ethers.provider.getBalance;
 
 
 describe("ERC721 contract", function() {
-  it("Deployment should not assign any Token to deployer", async function() {
-    const [owner,addr1, addr2] = await ethers.getSigners();
+  let owner, user1, user2
+  let myerc721
+
+  beforeEach(async function() {
+    ;[owner, user1, user2] = await ethers.getSigners();
 
     console.log('owner.address', owner.address)
-    console.log('addr1.address', addr1.address)
+    console.log('user1.address', user1.address)
 
     const contractfactory = await ethers.getContractFactory("MYERC721");
-    const myerc721 = await contractfactory.deploy("MYERC721","TEST","");
+    myerc721 = await contractfactory.deploy("MYERC721","TEST","");
 
     console.log('myerc721.address', myerc721.address);
+  })
 
-    const ownerBalance = await myerc721.balanceOf(owner.address);
-    expect("0").to.equal(ownerBalance);
+  it("Deployment should not assign any Token to deployer", async function() {
+    
+    expect(await myerc721.balanceOf(owner.address)).to.equal("0");
   });
 
 });
