aeb114a91691d834b1d7ab39909b7907660babc4
HA <>
2021-08-11 00:50:39 +0800

    test: simplify test code

--- basic/12-token-crowdfund/test/project-test.js
+++ basic/12-token-crowdfund/test/project-test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Project contract", function () {
     let projectContractFactory;
@@ -9,86 +10,38 @@ describe("Project contract", function () {
 
     beforeEach(async function () {
         [creator,donator1,donator2] = await ethers.getSigners();
-        console.log(creator.address)
         projectContractFactory = await ethers.getContractFactory("Project");
         const expiretime = parseInt(new Date().getTime()/1000) + 7 * 24 * 60 * 60
 
         projectContract = await projectContractFactory.deploy(creator.address,"Buy some toys","Buy some toys",expiretime,100);
-        await projectContract.deployed();
-        
-        expect(projectContract.address).to.not.equal(null);
-      });
 
-    describe('Start a new Project', function () {
-      // `it` is another Mocha function. This is the one you use to define your
-      // tests. It receives the test name, and a callback function.
+        expect(projectContract.address).to.not.be.null;
+    });
 
-      it('Get details of a new project', async function () {
+    it('Get details of a new project', async function () {
         let projectDetail = await projectContract.getDetails()
         expect(projectDetail.goalAmount).to.equal(100)
-      })
     })
 
-    describe('Check If Funding Complete Or Expired', function () {
-      // `it` is another Mocha function. This is the one you use to define your
-      // tests. It receives the test name, and a callback function.
-
-      it('Get the complete time', async function () {
+    it('Get the complete time', async function () {
         await projectContract.checkIfFundingCompleteOrExpired()
         expect(parseInt(await projectContract.completeAt())).to.not.equal(null);
-      })
     })
 
-    describe('Contribute to project ', function () {
-        // `it` is another Mocha function. This is the one you use to define your
-        // tests. It receives the test name, and a callback function.
+    it('Donate with calling contribute ', async function () {
+        const donation = { value: 10 }
 
-        it('Donate with calling contribute ', async function () {
-            let overrides = {
-                // To convert Ether to Wei:
-                value: 10,
-            }
+        await projectContract.connect(donator1).contribute(donation)
 
-            const artifact = artifacts.readArtifactSync("Project")
-            const transactionContract = new ethers.Contract(projectContract.address, artifact.abi, donator1);
-            await transactionContract.contribute(overrides)
-        
-            expect(parseInt(await transactionContract.currentBalance())).to.equal(10)
-        })
+        expect(parseInt(await projectContract.currentBalance())).to.equal(10)
     })
 
-    describe('Project successed when get enough donation', function () {
-        // `it` is another Mocha function. This is the one you use to define your
-        // tests. It receives the test name, and a callback function.
-        
-        
-        it('Donate 101', async function () {
-            let overrides = {
-                // To convert Ether to Wei:
-                value: 101,
-            }
+    it('Donate 101', async function () {
+        const donation = { value: 101 }
 
-            const artifact = artifacts.readArtifactSync("Project")
-            const transactionContract = new ethers.Contract(projectContract.address, artifact.abi, donator1);
-            await transactionContract.contribute(overrides)
-        
-            expect(parseInt(await transactionContract.state())).to.equal(2)
-        })
-    })
+        await projectContract.connect(donator1).contribute(donation)
 
-    describe('Creator get payout when donation success', function () {
-        it('Payout to the creator', async function () {
-            let overrides = {
-                // To convert Ether to Wei:
-                value: 101,
-                gasPrice: 1000000000
-            }
-
-            const artifact = artifacts.readArtifactSync("Project")
-            const transactionContract = new ethers.Contract(projectContract.address, artifact.abi, donator1);
-            await transactionContract.contribute(overrides)
-
-        })
+        expect(parseInt(await projectContract.state())).to.equal(2)
     })
 
 });
