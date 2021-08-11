61c38827e619e372f1b52db3c4082950643fdd7b
jolycao <jolycao@jolycaodeMacBook-Pro.local>
2021-08-11 08:54:46 +0800

    delete comment

--- defi/Uniswap-V3/graphGuid/scripts/fetchMints.js
+++ defi/Uniswap-V3/graphGuid/scripts/fetchMints.js
@@ -1,6 +1,3 @@
-/*
-There are two way to fetch data from TheGraph
-*/
 const { execute, makePromise } = require('apollo-link');
 const { HttpLink } = require('apollo-link-http');
 const gql = require('graphql-tag');
@@ -46,13 +43,10 @@ const link = new HttpLink({ uri, fetch });
 const operation = {
     query: query_position,
     variables, //optional
-    // operationName: {}, //optional
-    // context: {}, //optional
-    // extensions: {}, //optional
   };
 
 
-// define the mysql connection
+
 let connection = mysql.createConnection({
   host : sqlVariables.host,
   user : sqlVariables.user, 
@@ -68,11 +62,10 @@ connection.connect(function(err) {
   console.log('连接成功 id ' + connection.threadId);
 });
 
-// inital timestamp filter value
+
 var _timestamp_gt = 0;
 
 
-// 更新 trace 表中 timeStamp 的最新值
 function updateTrace(){
   connection.query('update trace set filter = ? where target = ?',[_timestamp_gt,'mints'], (err, results) => {
     if(err){
@@ -82,7 +75,7 @@ function updateTrace(){
   })
 }
 
-  // 开始抓取数据
+
 function fetchMore(){
     variables._timestamp_gt = _timestamp_gt
     console.log("Current TimeStamp: ",_timestamp_gt)
@@ -94,8 +87,7 @@ function fetchMore(){
           connection.end()
           process.exit(0)
         }
-      // 返回的 pools 是 object 类型, 需要进行遍历提取其中的 mints
-      // 同时, mints 也是 object 类型，需要进行第二层遍, 提取其中单个的 mint
+
         Object.keys(obj[0].mints).forEach(m => {
           connection.query('INSERT INTO mints(id,pool_id, mint_timestamp, owner, origin, amount, mint_amount0, mint_amount1, mint_amountUSD, mint_tickLower, mint_tickUpper, mint_transaction_id, mint_transaction_gasUsed, mint_transaction_gasPrice) \
               VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [obj[0].mints[m].id,obj[0].id, obj[0].mints[m].timestamp, obj[0].mints[m].owner, obj[0].mints[m].origin,parseInt(obj[0].mints[m].amount), parseFloat(obj[0].mints[m].amount0), parseFloat(obj[0].mints[m].amount1), parseFloat(obj[0].mints[m].amountUSD), parseInt(obj[0].mints[m].tickLower), parseInt(obj[0].mints[m].tickUpper), obj[0].mints[m].transaction.id,
@@ -105,13 +97,11 @@ function fetchMore(){
               }
             })
 
-            // 更新过滤条件 _timestamp_gt
             if(obj[0].mints[m].timestamp > _timestamp_gt){
               _timestamp_gt = parseInt(obj[0].mints[m].timestamp)
             }
         })
   
-        // 如果返回记录数不足 1000，说明没有后续记录，更新 trace 表后退出
         if(obj[0].mints.length != 1000){
           connection.query('update trace set filter = ? where target = ?',[_timestamp_gt,'mints'], (err, results) => {
             if(err){
@@ -121,7 +111,6 @@ function fetchMore(){
           connection.end()
           process.exit(0)
         }else{
-          // 否则，调用  updateTrace 函数进行循环处理
           updateTrace()
         }
       },
@@ -130,39 +119,10 @@ function fetchMore(){
     });
   }
 
-  
- // 查询跟踪表 trace 中记录的目前最大值
+
  module.exports.doMintsFetch = function(){  
   connection.query('select filter from trace where target = ?',['mints'],(err, results) => {
     _timestamp_gt = results[0].filter
     fetchMore()
   })
 }
-
-/*
-Second one, we can fetch data directly
-*/
-
-/* async function fetchData() {
-    const response = await fetch(uri, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `
-        query { factories(first: 5) {
-          id
-          poolCount
-          txCount
-          totalVolumeUSD
-        } }
-        `,
-      }),
-    });
-  
-    const responseBody = await response.json();
-    console.log(JSON.stringify(responseBody));
-  }
-  
-  fetchData(); */
