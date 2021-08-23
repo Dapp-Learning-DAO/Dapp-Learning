8a0874fea2d870834f85ca7af26d4842203eeb2e
HA <>
2021-08-23 12:54:06 +0800

    format code with prettier-standard vs plugin

--- basic/12-token-crowdfund/test/project-test.js
+++ basic/12-token-crowdfund/test/project-test.js
@@ -1,48 +1,52 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
-describe("Project contract", function () {
-    let projectContractFactory;
-    let projectContract;
-    let creator;
-    let donator1;
-    let donator2;
+describe('Project contract', function () {
+  let projectContractFactory
+  let projectContract
+  let creator
+  let donator1
+  let donator2
 
-    beforeEach(async function () {
-        [creator,donator1,donator2] = await ethers.getSigners();
-        projectContractFactory = await ethers.getContractFactory("Project");
-        const expiretime = parseInt(new Date().getTime()/1000) + 7 * 24 * 60 * 60
+  beforeEach(async function () {
+    ;[creator, donator1, donator2] = await ethers.getSigners()
+    projectContractFactory = await ethers.getContractFactory('Project')
+    const expiretime = parseInt(new Date().getTime() / 1000) + 7 * 24 * 60 * 60
 
-        projectContract = await projectContractFactory.deploy(creator.address,"Buy some toys","Buy some toys",expiretime,100);
+    projectContract = await projectContractFactory.deploy(
+      creator.address,
+      'Buy some toys',
+      'Buy some toys',
+      expiretime,
+      100
+    )
 
-        expect(projectContract.address).to.not.be.null;
-    });
+    expect(projectContract.address).to.not.be.null
+  })
 
-    it('Get details of a new project', async function () {
-        let projectDetail = await projectContract.getDetails()
-        expect(projectDetail.goalAmount).to.equal(100)
-    })
+  it('Get details of a new project', async function () {
+    let projectDetail = await projectContract.getDetails()
+    expect(projectDetail.goalAmount).to.equal(100)
+  })
 
-    it('Get the complete time', async function () {
-        await projectContract.checkIfFundingCompleteOrExpired()
-        expect(parseInt(await projectContract.completeAt())).to.not.equal(null);
-    })
+  it('Get the complete time', async function () {
+    await projectContract.checkIfFundingCompleteOrExpired()
+    expect(parseInt(await projectContract.completeAt())).to.not.equal(null)
+  })
 
-    it('Donate with calling contribute ', async function () {
-        const donation = { value: 10 }
+  it('Donate with calling contribute ', async function () {
+    const donation = { value: 10 }
 
-        await projectContract.connect(donator1).contribute(donation)
+    await projectContract.connect(donator1).contribute(donation)
 
-        expect(parseInt(await projectContract.currentBalance())).to.equal(10)
-    })
+    expect(parseInt(await projectContract.currentBalance())).to.equal(10)
+  })
 
-    it('Donate 101', async function () {
-        const donation = { value: 101 }
+  it('Donate 101', async function () {
+    const donation = { value: 101 }
 
-        await projectContract.connect(donator1).contribute(donation)
+    await projectContract.connect(donator1).contribute(donation)
 
-        expect(parseInt(await projectContract.state())).to.equal(2)
-    })
-
-});
-
+    expect(parseInt(await projectContract.state())).to.equal(2)
+  })
+})
