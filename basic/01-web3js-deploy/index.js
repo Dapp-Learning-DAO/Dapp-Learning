8da20fbc12f1c66a6ca71a331e36ddf7f90c365e
maroonstar <744428333@qq.com>
2021-09-15 21:03:29 +0800

    fixed win10 powershell run bug

--- basic/01-web3js-deploy/index.js
+++ basic/01-web3js-deploy/index.js
@@ -71,4 +71,9 @@ const Deploy = async () => {
   console.log(`Contract deployed at address: ${deployReceipt.contractAddress}`);
 };
 
-Deploy();
+Deploy()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
