f8e46520121c06f794b0b13b39707db68741c41f
jolycao <jolycao@jolycaodeMacBook-Pro.local>
2021-09-29 17:31:44 +0800

    fix readme of task 16

--- basic/16-nft-auction-exchange/react-app/src/App.jsx
+++ basic/16-nft-auction-exchange/react-app/src/App.jsx
@@ -283,7 +283,7 @@ function App (props) {
 
   
   let networkDisplay = ""
-  if (localChainId && selectedChainId && localChainId != selectedChainId) {
+  if (localChainId && selectedChainId && parseInt(localChainId) != parseInt(selectedChainId)) {
     networkDisplay = (
       <div style={{ zIndex: 2, position: 'absolute', right: 0, top: 60, padding: 16 }}>
         <Alert
