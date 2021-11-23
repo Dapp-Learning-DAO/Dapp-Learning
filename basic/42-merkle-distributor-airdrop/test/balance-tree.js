d002da5f41c2957bd4739fde1abb3f1dfbfb9a5f
sansx <646924078@qq.com>
2021-11-23 21:55:04 +0800

    translate files

--- basic/42-merkle-distributor-airdrop/test/balance-tree.js
+++ basic/42-merkle-distributor-airdrop/test/balance-tree.js
@@ -1,18 +1,18 @@
-const MerkleTree  =  require('./merkle-tree')
-const { BigNumber, utils } =  require( 'ethers')
+const MerkleTree = require('./merkle-tree')
+const { BigNumber, utils } = require('ethers')
 
-  let tree;
-  export default class BalanceTree {
-    
-   constructor(balances) {
-      this.tree = new MerkleTree(
-        balances.map(({ account, amount }, index) => {
-          return BalanceTree.toNode(index, account, amount)
-        })
-      )
-    }
+let tree;
+export default class BalanceTree {
 
-  function verifyProof(
+  constructor(balances) {
+    this.tree = new MerkleTree(
+      balances.map(({ account, amount }, index) => {
+        return BalanceTree.toNode(index, account, amount)
+      })
+    )
+  }
+
+  verifyProof(
     index,
     account,
     amount,
@@ -28,19 +28,19 @@ const { BigNumber, utils } =  require( 'ethers')
   }
 
   // keccak256(abi.encode(index, account, amount))
-  function toNode(index, account, amount) {
+  toNode(index, account, amount) {
     return Buffer.from(
       utils.solidityKeccak256(['uint256', 'address', 'uint256'], [index, account, amount]).substr(2),
       'hex'
     )
   }
 
-  function getHexRoot() {
+  getHexRoot() {
     return this.tree.getHexRoot()
   }
 
   // returns the hex bytes32 values of the proof
-  function getProof(index, account, amount) {
+  getProof(index, account, amount) {
     return this.tree.getHexProof(BalanceTree.toNode(index, account, amount))
   }
 
