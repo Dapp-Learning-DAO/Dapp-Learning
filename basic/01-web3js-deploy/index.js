9dbc919d52f246f687fe9859b11bb18a15979e0f
Meo <94157929+MeoWu630@users.noreply.github.com>
2021-12-31 11:27:11 +0100

    Update index.js

--- basic/01-web3js-deploy/index.js
+++ basic/01-web3js-deploy/index.js
@@ -2,7 +2,7 @@ let Web3 = require('web3');
 let solc = require('solc');
 let fs = require('fs');
 
-// Get privatekey from .env file
+// Get privatekey from environment
 require('dotenv').config();
 const privatekey = process.env.PRIVATE_KEY;
 
