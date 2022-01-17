3178c6a61cce9aa24042fa8f80ef46c0b8ce43c7
longdacao <longdacao@163.com>
2022-01-17 08:12:45 +0800

    fix contract

--- basic/28-optimism-layer2/contracts/L1ChugSplashProxy.sol
+++ basic/28-optimism-layer2/contracts/L1ChugSplashProxy.sol
@@ -1,20 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.5.0 <0.8.0;
 
-/**
- * @title L1ChugSplashProxy
- * @dev Basic ChugSplash proxy contract for L1. Very close to being a normal proxy but has added
- * functions `setCode` and `setStorage` for changing the code or storage of the contract. Nifty!
- *
- * Note for future developers: do NOT make anything in this contract 'public' unless you know what
- * you're doing. Anything public can potentially have a function signature that conflicts with a
- * signature attached to the implementation contract. Public functions SHOULD always have the
- * 'proxyCallIfNotOwner' modifier unless there's some *really* good reason not to have that
- * modifier. And there almost certainly is not a good reason to not have that modifier. Beware!
- */
-contract L1ChugSplashProxy {
+interface L1ChugSplashProxy {
 
-    function depositETH(uint32 _l2Gas, bytes memory _data) public payable {
-
-    }
+    function depositETH(uint32 _l2Gas, bytes memory _data) external payable;
 }
