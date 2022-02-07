612eeb8117fe28cfd41686442784d1878e23e75d
maroonstar <744428333@qq.com>
2022-02-07 13:26:56 +0800

    task05

--- basic/09-hardhat-react/contracts/SimpleToken.sol
+++ basic/09-hardhat-react/contracts/SimpleToken.sol
@@ -1,32 +1,114 @@
-//  2019/10/15  //0xd2b7915d17e90771afd042ad403999167bf9abf8
-
-// 用0。5。6编译器
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./presets/ERC20PresetMinterPauser.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol';
+import '@openzeppelin/contracts/access/AccessControlEnumerable.sol';
+import '@openzeppelin/contracts/utils/Context.sol';
 
+/**
+ * @dev {ERC20} token, including:
+ *
+ *  - ability for holders to burn (destroy) their tokens
+ *  - a minter role that allows for token minting (creation)
+ *  - a pauser role that allows to stop all token transfers
+ *
+ * This contract uses {AccessControl} to lock permissioned functions using the
+ * different roles - head to its documentation for details.
+ *
+ * The account that deploys the contract will be granted the minter and pauser
+ * roles, as well as the default admin role, which will let it grant both minter
+ * and pauser roles to other accounts.
+ */
+contract ERC20PresetMinterPauser is Context, AccessControlEnumerable, ERC20Burnable, ERC20Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant PAUSER_ROLE = keccak256('PAUSER_ROLE');
+
+    /**
+     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
+     * account that deploys the contract.
+     *
+     * See {ERC20-constructor}.
+     */
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+    }
+
+    /**
+     * @dev Creates `amount` new tokens for `to`.
+     *
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), 'ERC20PresetMinterPauser: must have minter role to mint');
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Pauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_pause}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function pause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), 'ERC20PresetMinterPauser: must have pauser role to pause');
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_unpause}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function unpause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), 'ERC20PresetMinterPauser: must have pauser role to unpause');
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
 
 contract SimpleToken is ERC20PresetMinterPauser {
-
-
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
 
     uint8 private _decimals;
-    uint256 public  INITIAL_SUPPLY = 10000 * (10 ** uint256(18));
+    uint256 public INITIAL_SUPPLY = 10000 * (10**uint256(18));
 
-
-    function decimals() public view  override returns (uint8) {
+    function decimals() public view override returns (uint8) {
         return _decimals;
     }
 
-    constructor(string memory name, string memory symbol, uint8  decimals, uint256  initial_supply) public  ERC20PresetMinterPauser(name, symbol){
-        
-    
-        _decimals = decimals;
-        INITIAL_SUPPLY = initial_supply * (10 ** uint256(decimals)) ;
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_,
+        uint256 initial_supply
+    ) public ERC20PresetMinterPauser(name, symbol) {
+        _decimals = decimals_;
+        INITIAL_SUPPLY = initial_supply * (10**uint256(decimals_));
         _mint(msg.sender, INITIAL_SUPPLY);
     }
-
 }
