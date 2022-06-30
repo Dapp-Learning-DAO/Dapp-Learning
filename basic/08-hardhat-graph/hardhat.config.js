30eda3a6e476d8f6c25ad2cc6c2effcc7840e8f8
yingjingyang <longdacao2@163.com>
2022-06-30 22:01:04 +0800

    fix config

--- basic/08-hardhat-graph/hardhat.config.js
+++ basic/08-hardhat-graph/hardhat.config.js
@@ -3,11 +3,8 @@ const fs = require('fs');
 require('dotenv').config();
 
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const defaultNetwork = 'localhost';
-
-function mnemonic() {
-  return process.env.PRIVATE_KEY;
-}
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -25,19 +22,19 @@ module.exports = {
     },
     rinkeby: {
       url: 'https://rinkeby.infura.io/v3/' + process.env.INFURA_ID, //<---- YOUR INFURA ID! (or it won't work)
-      accounts: [mnemonic()],
+      accounts: [PRIVATE_KEY],
     },
     kovan: {
       url: 'https://kovan.infura.io/v3/' + process.env.INFURA_ID, //<---- YOUR INFURA ID! (or it won't work)
-      accounts: [mnemonic()],
+      accounts: [PRIVATE_KEY],
     },
     mainnet: {
       url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_ID, //<---- YOUR INFURA ID! (or it won't work)
-      accounts: [mnemonic()],
+      accounts: [PRIVATE_KEY],
     },
     ropsten: {
       url: 'https://ropsten.infura.io/v3/' + process.env.INFURA_ID, //<---- YOUR INFURA ID! (or it won't work)
-      accounts: [mnemonic()],
+      accounts: [PRIVATE_KEY],
     },
   },
 };
