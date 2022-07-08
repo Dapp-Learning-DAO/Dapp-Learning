e11f3f6d8d5c130ba572863e23bdd37286dc6fb2
masonhunk <aoronchu@gmail.com>
2022-07-08 20:06:22 +0800

    .

--- security/1-reentrancy/uniAndImbtc/contracts/UniV1Simple.sol
+++ security/1-reentrancy/uniAndImbtc/contracts/UniV1Simple.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract UniV1Simple {
+contract UniV1Simple is ERC20{
 
     IERC20 private erc20;
 
-    constructor(address erc20_) {
+    constructor(address erc20_) ERC20("TEST", "TEST"){
         erc20 = IERC20(erc20_);
     }
 
@@ -17,7 +18,7 @@ contract UniV1Simple {
         uint256 token_reserve = erc20.balanceOf(address(this));
         uint256 token_bought = getInputPrice(eth_sold, eth_reserve, token_reserve);
         require(token_bought >= min_tokens, "UniV1: Not enough");
-        require(erc20.transferFrom(address(this), msg.sender, token_bought), "UniV1: Transfer failed");
+        require(erc20.transfer(msg.sender, token_bought), "UniV1: Transfer failed");
     }
 
     //take ETH as input ,get token as output. Specify how much you bought
@@ -29,12 +30,11 @@ contract UniV1Simple {
         if (msg.value > eth_sold) {
             payable(msg.sender).transfer(msg.value - eth_sold);
         }
-        require(erc20.transferFrom(address(this), msg.sender, token_bought), "UniV1: Transfer failed");
+        require(erc20.transfer(msg.sender, token_bought), "UniV1: Transfer failed");
     }
 
     //take token as input, get eth as output. Specify how much input
     function tokenToEthInput(uint256 token_sold) external {
-        require(erc20.allowance(msg.sender, address(this)) >= token_sold, "UniV1: Not approved");
         uint256 eth_reserve = address(this).balance;
         uint256 token_reserve = erc20.balanceOf(address(this));
         uint256 eth_bought = getInputPrice(token_sold, token_reserve, eth_reserve);
@@ -46,8 +46,34 @@ contract UniV1Simple {
         uint256 token_reserve = erc20.balanceOf(address(this));
         uint256 eth_reserve = address(this).balance;
         uint256 token_sold = getOutputPrice(eth_bought, token_reserve, eth_reserve);
-        require(erc20.transferFrom(msg.sender, address(this), token_sold), "UniV1: Transfer failed");
         payable(msg.sender).transfer(eth_bought);
+        require(erc20.transferFrom(msg.sender, address(this), token_sold), "UniV1: Transfer failed");
+    }
+
+    function addLiquidity(uint256 max_token) external payable returns(uint256){
+        if (totalSupply() > 0) {
+            uint256 eth_reserve = address(this).balance - msg.value;
+            uint256 token_reserve = erc20.balanceOf(address(this));
+            uint256 actual_token = (msg.value * token_reserve / eth_reserve) + 1;
+            require(actual_token <= max_token, "UniV1: Exceed limit");
+            require(erc20.transferFrom(msg.sender, address(this), actual_token), "UniV1: Transfer failed");
+            
+            uint256 liquid_minted = msg.value * totalSupply() / eth_reserve;
+            _mint(msg.sender, liquid_minted);
+            return liquid_minted;
+            
+        } else {
+            //mint initial uni tokens. how many?
+            //transfer erc 20 token (eth already added to balance)
+            uint256 initial_mint = address(this).balance;
+            _mint(msg.sender, initial_mint);
+            require(erc20.transferFrom(msg.sender, address(this), max_token), "UniV1: Transfer failed");
+            return initial_mint;
+        }
+    }
+
+    function removeLiquidity(uint256 uni_amount) external {
+
     }
 
     //(x+Δx) (y-Δy) = xy
@@ -63,8 +89,8 @@ contract UniV1Simple {
 
 
     function getOutputPrice(uint256 output_amount, uint256 input_reserve, uint256 output_reserve) internal pure returns(uint256) {
-        uint256 nominator = input_reserve * output_amount;
-        uint256 denominator = output_reserve - output_amount;
+        uint256 nominator = input_reserve * output_amount * 1000;
+        uint256 denominator = (output_reserve - output_amount) * 997;
         return nominator / denominator;
     }
 
