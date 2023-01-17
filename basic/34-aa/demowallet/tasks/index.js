fb4636f3f07b6ab718f8352f166d1479fefad4c2
arc0035 <aoronchu@gmail.com>
2023-01-17 16:51:58 +0800

    .

--- basic/34-aa/demowallet/tasks/index.js
+++
@@ -1,7 +0,0 @@
-require("./1_deploy_account");
-require("./2_fund_account");
-require("./3_send_userop");
-require("./test");
-// module.exports = {
-    
-// }
--- basic/34-aa/demowallet/tasks/test.js
+++
@@ -1,9 +0,0 @@
-const {task} = require("hardhat/config");
-const { EntryPoint__factory} = require('@account-abstraction/contracts');
-const { ethers } = require("ethers");
-const ABI = EntryPoint__factory.abi;
-
-task("ttt", "")
-  .setAction(async (taskArgs) => {
-    ethers.providers.JsonRpcProvider
-  });
