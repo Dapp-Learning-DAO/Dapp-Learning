44afff153a4caede8d9caaffbaeee360d959f3ab
hotaplayer <15000810224@139.com>
2023-08-10 00:00:24 +0800

    feat(wc_v2):enrich basic impelementation

--- basic/78-wallet-connect/v2/basic-example/src/App.js
+++ basic/78-wallet-connect/v2/basic-example/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
 import './App.css';
+//wagmi dependencies
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+//view dependencies
+import { localhost, polygon } from '@wagmi/chains'
+
+/*Firstly we create the client calling to public JSON rpcs.
+  You can refer to https://wagmi.sh/react/providers/configuring-chains
+  Also keep in mind that publicClient is the http client connecting to public JSON-RPC endpoints while webSocketPublicClient 
+  is websocket client.
+*/
+const { chains, publicClient, webSocketPublicClient ,} = configureChains(
+  //more chains see https://www.npmjs.com/package/@wagmi/chains
+  [polygon,localhost], 
+  //providers are tried one by one to connect to above chains. more to see https://wagmi.sh/react/providers/configuring-chains
+  //publicProvider will get the url from the preconfigured chains
+  [publicProvider(), jsonRpcProvider({rpc: (chain) => ({http: 'https://localhost:8545',}),})]
+)
+ 
+/*Create wagmi config
+*/
+const config = createConfig({
+  connectors:[
+    new MetaMaskConnector({chains}), 
+    // new WalletConnectConnector({
+    //   chains,
+    //   options: {
+    //     projectId: '...',
+    //   },
+    // }),
+    //See https://wagmi.sh/examples/connect-wallet for more
+  ],
+  publicClient, 
+  webSocketPublicClient
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <WagmiConfig config={config}>
+        <Wallet />
+    </WagmiConfig>
+  )
+}
+
+const Wallet = ()=> {
+  const { address, connector, isConnected } = useAccount();
+  const { connect, connectors } = useConnect();
+  const { disconnect } = useDisconnect()
+  
+  if (isConnected)
+    return (
+      <div className='profile'>
+        <div>Connector:{connector.name}</div>
+        <div>Current address:{address}</div>
+        <div>
+          <Button variant="outlined" color="primary" onClick={() => disconnect()}>
+            Disconnect
+          </Button>
+        </div>
+      </div>
+    )
+    return (<div className='profile'>
+      {connectors.map((c) => (
+        <Button style={{
+          marginBottom: '20px'
+        }} 
+        variant="contained" color="primary" onClick={() => connect({connector:c})}>
+        Connect {c.name}
+        </Button>
+      ))}
+
+    </div>) 
+    
 }
 
 export default App;
--- basic/78-wallet-connect/v2/basic-example/src/index.js
+++ basic/78-wallet-connect/v2/basic-example/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +10,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
