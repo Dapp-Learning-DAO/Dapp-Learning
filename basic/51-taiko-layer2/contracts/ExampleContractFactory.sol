e814164bdca7f35a34b8764bbd3143fc556bfae8
liberhe <yanyanho126@gmail.com>
2023-09-17 15:03:07 +0800

    fix the create2 bug

--- basic/51-taiko-layer2/contracts/ExampleContractFactory.sol
+++ basic/51-taiko-layer2/contracts/ExampleContractFactory.sol
@@ -22,7 +22,7 @@ contract ExampleContractFactory {
         uint _salt
     ) public view returns (address) {
         bytes32 hash = keccak256(
-            abi.encodePacked(bytes1(0xff), msg.sender, _salt, keccak256(bytecode))
+            abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(bytecode))
         );
 
         // NOTE: cast last 20 bytes of hash to address
