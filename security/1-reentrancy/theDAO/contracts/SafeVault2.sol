997249429f00c2832d8694017fc47002bba1c2f1
Luna <shyufeng2163sh.net@qq.com>
2024-10-26 13:06:00 +0800

    perfs: optmize the dao contracts

--- security/1-reentrancy/theDAO/contracts/SafeVault2.sol
+++ security/1-reentrancy/theDAO/contracts/SafeVault2.sol
@@ -1,24 +1,27 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-
 
 import "./IVault.sol";
 import "hardhat/console.sol";
 
 contract SafeVault2 is IVault {
-
-    mapping(address=>uint256) public balances;
+    mapping(address => uint256) public balances;
     
-    function deposit() external override payable{
+    function deposit() external override payable {
         balances[msg.sender] += msg.value;
     }
 
     function withdraw() external override {
-        //Checks
-        require(balances[msg.sender] > 0, "Insufficient money");
-        //Effects
+        // Checks
+        require(balances[msg.sender] > 0, "Insufficient balance");
+
+        // Effects
+        uint256 amount = balances[msg.sender];
         balances[msg.sender] = 0;
-        //Interact
+
+        // Interact
         address payable target = payable(msg.sender);
-        (bool success,) = target.call{value:balances[msg.sender]}("");
+        (bool success, ) = target.call{value: amount}("");
+        require(success, "Transfer failed");
     }
 }
